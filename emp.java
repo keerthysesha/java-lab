29b1471d91cd7415ff9a30971c64ef21728ae581
keerthysesha <125575936+keerthysesha@users.noreply.github.com>
2023-06-04 23:25:33 +0530

    emp.java

--- emp.java
+++ emp.java
@@ -1,6 +1,15 @@
+AIM:
+	To write a java program to print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'. 
+        
+PROCEDURE:
+    STEP-1: Import the Java Scanner to get input from the user.
+    STEP-2: Declare the variables that we are about to use.
+    STEP-3: Instantiate the Scanner class.
+    STEP-4: Get the details of employees as input from the user.
+    STEP-5: print the employee information.
+    Step-6: Close the object of the Scanner class.
 
-
-
+CODE:
 public class emp1
 {
     public String empName(String name)
@@ -50,3 +59,23 @@ public class emp extends emp1
 
     }
 }
+
+OUTPUT:
+PS C:\Users\Keerthy Seshasayanam\OneDrive\Desktop\java> javac emp.java
+PS C:\Users\Keerthy Seshasayanam\OneDrive\Desktop\java> java emp      
+Enter the Name of the employee:
+Robert
+Sam
+John
+Enter the Year of Joining:
+1994
+2000
+1999
+Enter the Address of the employee:
+64C-WallsStreat
+68D-WallsStreat
+26B-WallsStreat
+Name            YearOfJoining   Address
+Robert          1994            64C-WallsStreat
+Sam             2000            68D- WallsStreat
+John            1999            26B- WallsStreat
